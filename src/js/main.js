6701e476966318da1c9658e3274b01dd575ba7e1
SanJeics <58487003+SanJeics@users.noreply.github.com>
2020-05-22 18:46:30 -0600

    f

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'; // jQuery import
 window.jQuery = $;
 import addFlexSlider from './jquery.flexslider'
+import {amazonScroller} from './jquery.flexslider'
 
 addFlexSlider($)
 
@@ -12,6 +13,26 @@ $(document).ready(function(){
 		pauseOnAction: false,
 		slideshowSpeed: 3000,
 		touch: true
+	});
+
+});
+
+amazonScroller($)
+$(function() {
+
+	$("#amazon_scroller2").amazon_scroller({
+		scroller_title_show: 'disable',
+		scroller_time_interval: '3000',
+		scroller_window_background_color: "none",
+		scroller_window_padding: '10',
+		scroller_border_size: '0',
+		scroller_border_color: '#CCC',
+		scroller_images_width: '100',
+		scroller_images_height: '80',
+		scroller_title_size: '12',
+		scroller_title_color: 'black',
+		scroller_show_count: '20',
+		directory: 'images'
 	});
 
 });
